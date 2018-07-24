92916bdda0601c3419d38d14df7bd2dba6dc6261
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-24 19:31:26 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -30,7 +30,7 @@ class Boat < ActiveRecord::Base
 
   def self.with_three_classifications
     binding.pry
-    self.includes(:classifications).where('classifications.count > ?', 3)
+    self.includes(:classifications).where(self.classifications.count > 3)
   end
 
 end
