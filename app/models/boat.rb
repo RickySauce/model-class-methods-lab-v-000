0d0221c87f922dffa2579372ee9c2ac5ffda12e7
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-24 20:27:48 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -29,7 +29,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    self.joins(:classifications).group('boats.id').having('count > ?', 3)
+    self.joins(:classifications).group('boats.id').having('count = 3')
   end
 
 end
